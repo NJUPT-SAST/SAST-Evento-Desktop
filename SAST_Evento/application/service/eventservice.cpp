66383f3daaaf8a83cf314b7c5f7536090f95546c
Serein <118594118+Serein207@users.noreply.github.com>
2023-08-09 20:19:08 +0800

    Update

--- SAST_Evento/application/service/eventservice.cpp
+++ SAST_Evento/application/service/eventservice.cpp
@@ -1,5 +1,10 @@
 #include "eventservice.h"
 
+Evento EventService::loadEvento(int eventId)
+{
+    return m_repository->evento(eventId);
+}
+
 std::vector<UndertakingEvento> EventService::loadUndertakingEvento()
 {
     return m_repository->undertakingEventoModel();
