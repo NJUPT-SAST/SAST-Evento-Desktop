d6eb0c534079ded7b2bdfcd3f9f0806ad2476343
Serein <118594118+Serein207@users.noreply.github.com>
2023-08-24 21:05:39 +0800

    Implement EventoEdit Controller

--- src/infrastructure/helper/evento_edit_helper.cpp
+++ src/infrastructure/helper/evento_edit_helper.cpp
@@ -34,4 +34,16 @@ void EventoEditHelper::updateEventoEdit(const QString &departmentJson, const QSt
     setProperty("departmentIds", m_departmentIds);
     setProperty("typeId", evento.type.id);
     setProperty("allowConflict", evento.type.allowConflict);
+    setProperty("eventDateStart", evento.gmtEventStart.toString(QStringLiteral("yyyy-MM-dd")));
+    setProperty("eventTimeStartH", evento.gmtEventStart.toString(QStringLiteral("hh")));
+    setProperty("eventTimeStartM", evento.gmtEventStart.toString(QStringLiteral("mm")));
+    setProperty("eventDateEnd", evento.gmtEventEnd.toString(QStringLiteral("yyyy-MM-dd")));
+    setProperty("eventTimeEndH", evento.gmtEventEnd.toString(QStringLiteral("hh")));
+    setProperty("eventTimeEndM", evento.gmtEventEnd.toString(QStringLiteral("mm")));
+    setProperty("registerDateStart", evento.gmtRegistrationStart.toString(QStringLiteral("yyyy-MM-dd")));
+    setProperty("registerTimeStartH", evento.gmtRegistrationStart.toString(QStringLiteral("hh")));
+    setProperty("registerTimeStartM", evento.gmtRegistrationStart.toString(QStringLiteral("mm")));
+    setProperty("registerDateEnd", evento.gmtRegistrationEnd.toString(QStringLiteral("yyyy-MM-dd")));
+    setProperty("registerTimeEndH", evento.gmtRegistrationEnd.toString(QStringLiteral("hh")));
+    setProperty("registerTimeEndM", evento.gmtRegistrationEnd.toString(QStringLiteral("mm")));
 }
