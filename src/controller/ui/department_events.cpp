a0b61e36808d1f35f2f8ed46ce9f233a8b7bd8b2
Serein <118594118+Serein207@users.noreply.github.com>
2023-08-26 01:05:47 +0800

    Complete controller department events (#45)
    * update controller
    
    ---------
    
    authored-by: Stary112 <1359821373@qq.com>
    Co-Authored-By: Stary112 <107787987+ssttyy123@users.noreply.github.com>

--- src/controller/ui/department_events.cpp
+++ src/controller/ui/department_events.cpp
@@ -1,14 +1,44 @@
 #include "department_events.h"
 #include "repository.h"
+#include "convertor.h"
+#include "evento_brief_model.h"
 
 QString DepartmentEventsController::loadDepartmentsInfo()
 {
     EventoException err;
     auto departmentList = getRepo()->get_department_list(err);
     if ((int)err.code()){
+        emit loadDepartmentsErrorEvent(err.message());
         return QString();
     }
+    emit loadDepartmentsSuccessEvent();
     return departmentList;
+}
+
+QString DepartmentEventsController::loadSubscribedDepartment()
+{
+    emit loadSubscribedDepartmentsSuccessEvent();
+    return "[1]";
+}
+
+void DepartmentEventsController::loadDepartmentEvents(int departmentId)
+{
+    EventoException err;
+    EventoBriefModel::getInstance()->resetModel(
+        Convertor<std::vector<DTO_Evento>, std::vector<EventoBrief>>()(
+            getRepo()->get_department_event_list(departmentId, err)));
+
+    if ((int)err.code()) {
+        emit loadDepartmentEventErrorEvent(err.message());
+        return;
+    }
+
+    emit loadDepartmentEventSuccessEvent();
+}
+
+void DepartmentEventsController::subscribeDepartment(bool check, int departmentId)
+{
+    emit subscribeSuccessEvent(check, departmentId);
 }
 
 DepartmentEventsController *DepartmentEventsController::create(QQmlEngine *qmlEngine, QJSEngine *jsEngine)
