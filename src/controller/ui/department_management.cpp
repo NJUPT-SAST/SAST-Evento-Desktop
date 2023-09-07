5a46ee8e989721e2dfa28c0d98c788af9e72d18d
Stary112 <1359821373@qq.com>
2023-09-07 12:48:28 +0800

    feature:LocationManagement (#65)
    * feature:LocationManagement
    
    * Update SAST_Evento.qrc

--- src/controller/ui/department_management.cpp
+++ src/controller/ui/department_management.cpp
@@ -13,17 +13,20 @@ QString DepartmentManagementController::loadAllDepartment()
     return res;
 }
 
-void DepartmentManagementController::deleteDepartment(const int typeId)
+void DepartmentManagementController::deleteDepartment(const int departmentId)
 {
+    // TODO 删除部门操作
     return emit deleteDepartmentSuccess();
 }
 
 void DepartmentManagementController::changeDepartmentName(int id, const QString name)
 {
+    // TODO 更改部门操作
     return emit changeDepartmentNameSuccess();
 }
 
 void DepartmentManagementController::addDepartment(const QString name)
 {
+    // TODO 添加部门操作
     return emit addDepartmentSuccess();
 }
---
+++ src/controller/ui/location_management.cpp
@@ -0,0 +1,32 @@
+#include "location_management.h"
+#include "repository.h"
+
+QString LocationManagementController::loadAllLocation()
+{
+    EventoException err;
+    QString res = getRepo()->get_location_list(err);
+    if((int)err.code()) {
+        emit loadAllLocationError(err.message());
+        return QString();
+    }
+    emit loadAllLocationSuccess();
+    return res;
+}
+
+void LocationManagementController::deleteLocation(const int locationId)
+{
+    // TODO 删除地点操作
+    return emit deleteTLocationSuccess();
+}
+
+void LocationManagementController::changeLocationName(int id, const QString name)
+{
+    // TODO 更改地点操作（谨慎使用）
+    return emit changeLocationNameSuccess();
+}
+
+void LocationManagementController::addLocation(const QString name, const int parentId)
+{
+    // TODO 添加地点操作
+    return emit addLocationSuccess();
+}
