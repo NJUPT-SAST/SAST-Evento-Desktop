c4b16002ce663cf0030c4513e9c55bb4c5345565
Stary112 <1359821373@qq.com>
2023-09-20 22:08:30 +0800

    Accomplish Slide Service (#78)
    创建slide service

--- src/controller/slide_service.cpp
+++ src/controller/slide_service.cpp
@@ -1,5 +1,27 @@
 #include "slide_service.h"
 #include "slide_model.h"
 #include "dto/slide.h"
+#include "plaza.h"
+#include "repository.h"
 
 Slide::Slide(const DTO_Slide& src) : id(src.id), title(src.title), link(src.link), url(src.url) {}
+
+void SlideService::load_HomeSlide(const int size)
+{
+    getRepo()->getHomeSlideList(size).then([=](EventoResult<std::vector<DTO_Slide>> result) {
+        if (!result)
+            PlazaController::getInstance()->onHomeSlideLoadFailure(result.message());
+        auto data = result.take();
+        std::vector<Slide> model;
+        {
+            std::lock_guard lock(mutex);
+            slides.clear();
+            for(auto& i : data) {
+                slides.push_back(Slide(i));
+                model.push_back(Slide(i));
+            }
+        }
+        SlideModel::getInstance()->resetModel(std::move(model));
+        PlazaController::getInstance()->onHomeSlideLoadFinished();
+    });
+}
--- src/controller/slide_service.h
+++ src/controller/slide_service.h
@@ -1,10 +1,22 @@
 #ifndef SLIDESERVICE_H
 #define SLIDESERVICE_H
 
+#include <slide.h>
+#include <shared_mutex>
+
 class SlideService
 {
-public:
+    std::shared_mutex mutex;
+    std::vector<Slide> slides;
+private:
     SlideService() = default;
+public:
+    static SlideService& getInstance() {
+        static SlideService singleton;
+        return singleton;
+    }
+
+    void load_HomeSlide(const int size);
 };
 
 #endif // SLIDESERVICE_H
