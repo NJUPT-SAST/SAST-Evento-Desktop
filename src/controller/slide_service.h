639a46ad6e109a6c0412125a00b55e7f3acfd9f0
ArcticLampyrid <ArcticLampyrid@outlook.com>
2023-10-04 23:21:38 +0800

    chore: ci linter test

--- src/controller/slide_service.h
+++ src/controller/slide_service.h
@@ -1,25 +1,15 @@
 #ifndef SLIDESERVICE_H
 #define SLIDESERVICE_H
-
 #include "slide.h"
-
 #include <shared_mutex>
 #include <vector>
-
 class SlideService {
-    std::shared_mutex mutex;
-    std::vector<Slide> slides;
-
+std::shared_mutex mutex;
+std::vector<Slide> slides;
 private:
-    SlideService() = default;
-
+SlideService() = default;
 public:
-    static SlideService& getInstance() {
-        static SlideService singleton;
-        return singleton;
-    }
-
-    void load_HomeSlide(const int size);
+static SlideService& getInstance() {static SlideService singleton;return singleton;}
+void load_HomeSlide(   const   int   size);
 };
-
 #endif // SLIDESERVICE_H
