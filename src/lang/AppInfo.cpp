f99ff07b087334ba75b5d5135eeac13a0b32c812
Serein <2075337935@qq.com>
2023-12-07 23:08:36 +0800

    chore: update version

--- src/lang/AppInfo.cpp
+++ src/lang/AppInfo.cpp
@@ -8,7 +8,7 @@
 #include <QtGlobal>
 
 AppInfo::AppInfo(QObject* parent) : QObject{parent} {
-    version("v0.4.1117-alpha");
+    version("v0.5.1207-alpha");
     if (QLocale::system().language() == QLocale::Chinese)
         lang(new Zh());
     else
