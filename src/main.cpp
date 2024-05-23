dbabb593fb67f366f9400ed5d8948ea5a3a538f4
ArcticLampyrid <ArcticLampyrid@outlook.com>
2024-05-23 12:34:00 +0800

    fix: link to QWidgets (close #138)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
+#include <QApplication>
 #include <QFileInfo>
 #include <QFontDatabase>
-#include <QGuiApplication>
 #include <QIcon>
 #include <QProcess>
 #include <QQmlApplicationEngine>
@@ -12,24 +12,24 @@
 #include "lang/AppInfo.h"
 
 int main(int argc, char* argv[]) {
-    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
+    QApplication::setHighDpiScaleFactorRoundingPolicy(
         Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
     qputenv("QT_QUICK_CONTROLS_STYLE", "Basic");
-    QGuiApplication::setOrganizationName("NJUPT-SAST-C++");
-    QGuiApplication::setOrganizationDomain("https://github.com/NJUPT-SAST-Cpp");
-    QGuiApplication::setApplicationName("SAST Evento Desktop");
+    QApplication::setOrganizationName("NJUPT-SAST-C++");
+    QApplication::setOrganizationDomain("https://github.com/NJUPT-SAST-Cpp");
+    QApplication::setApplicationName("SAST Evento Desktop");
 
     SettingsHelper::getInstance()->init(argv);
     if (SettingsHelper::getInstance()->getRender() == "software") {
         QQuickWindow::setGraphicsApi(QSGRendererInterface::Software);
     }
 
-    QGuiApplication app(argc, argv);
-    QGuiApplication::setWindowIcon(QIcon(QStringLiteral(":/app.ico")));
+    QApplication app(argc, argv);
+    QApplication::setWindowIcon(QIcon(QStringLiteral(":/app.ico")));
     auto fontId = QFontDatabase::addApplicationFont(":/res/font/MiSans-Regular.ttf");
     QStringList fontFamilies = QFontDatabase::applicationFontFamilies(fontId);
     if (!fontFamilies.empty())
-        QGuiApplication::setFont(QFont(fontFamilies[0]));
+        QApplication::setFont(QFont(fontFamilies[0]));
 
     auto* appInfo = new AppInfo();
     QQmlApplicationEngine engine;
@@ -43,9 +43,9 @@ int main(int argc, char* argv[]) {
                 QCoreApplication::exit(-1);
         },
         Qt::QueuedConnection);
-    QObject::connect(&engine, &QQmlApplicationEngine::quit, &app, &QGuiApplication::quit);
+    QObject::connect(&engine, &QQmlApplicationEngine::quit, &app, &QApplication::quit);
     engine.load(url);
-    const int exec = QGuiApplication::exec();
+    const int exec = QApplication::exec();
     if (exec == 931) {
         QProcess::startDetached(qApp->applicationFilePath(), QStringList());
     }
